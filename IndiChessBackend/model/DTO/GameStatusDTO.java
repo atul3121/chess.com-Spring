8d6b42a4a3e6ae100bd72e5c21d429d2bccecd97
Atul Raj <119055533+atul3121@users.noreply.github.com>
2026-02-02 12:31:25 +0530

    Add files via upload

---
+++ IndiChessBackend/model/DTO/GameStatusDTO.java
@@ -0,0 +1,18 @@
+
+package com.example.IndiChessBackend.model.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameStatusDTO {
+    private String status;
+    private Long matchId;
+    private String playerColor;
+    private boolean isMyTurn;
+    private String[][] board;
+    private String fen;
+}
